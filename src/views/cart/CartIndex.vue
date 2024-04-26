77bcb04030bcedf61da1980d88d2c1d27d75a913
Mochammad Gamal <mogams1811@gmail.com>
2024-04-26 13:55:53 +0700

    feat: :ambulance: connect restful delete item product from cart api to this cart page

--- src/views/cart/CartIndex.vue
+++ src/views/cart/CartIndex.vue
@@ -6,6 +6,7 @@
         v-for="item in cartItems"
         :key="item.id"
         :item="item"
+        v-on:remove-item="removeFromCart($event)"
       />
       <h3 id="total-price"> Total: Rp. {{ totalPrice }}</h3>
       <button id="checkout-button">Checkout</button>
@@ -32,6 +33,17 @@ export default {
         (sum, item) => sum + Number(item.price),
         0
       )
+    }
+  },
+  methods: {
+    async removeFromCart(product) {
+      await axios.delete(
+        `http://localhost:8081/api/orders/user/1/product/${product}`
+      );
+      let indexCart = this.cartItems.map(function(item) {
+        return item.code
+      }).indexOf(product);
+      this.cartItems.splice(indexCart, 1);
     }
   },
   async created() {
